3215daff993837e4dce8a6acc6260af50c11e631
MrMeghoo <mikelmeghoo@gmail.com>
2024-03-25 11:55:12 -0400

    second Api(update)

--- SecondApi.js
+++ SecondApi.js
@@ -1,3 +1,5 @@
 // The second API was fetched using the ticketmaster deveploper tool
+//This is the link is a reference for the parameters:
+//https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/
 
 fetch('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=comedy&postalCode=30303&apikey=pjOyZm2wY8wpDMijKrvkbYyzGxigNnZH')
