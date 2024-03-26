731464b6882bb93f33c810129981ed83ca6b7e88
COR Coding <corcoding@DESKTOP-QMOVT9L.localdomain>
2024-03-26 14:42:57 -0400

    added animation

---
+++ testP.js
@@ -0,0 +1,29 @@
+function dave(){
+anime({
+    targets: '#DaveChappelle',
+    translateX: [-100],
+    delay: 500,
+    direction: 'alternate',
+    loop: 1,
+    duration:2000
+    
+  });
+  anime({
+    targets: '#Katwilliams',
+    translateX: [-300],
+    delay: 500,
+    direction: 'alternate',
+    loop: 1,
+    duration:2000
+    
+  });
+  anime({
+    targets: '#EddieMurphy',
+    translateX: [-900],
+    delay: 500,
+    direction: 'alternate',
+    loop: 1,
+    duration:2000
+    
+  });
+}
