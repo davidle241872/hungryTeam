acd45c3e24e4c37447553f9b22425cb3c7312b8a
COR Coding <corcoding@DESKTOP-QMOVT9L.localdomain>
2024-03-26 15:38:57 -0400

    finished animation with comedians

--- testP.js
+++ testP.js
@@ -26,4 +26,103 @@ anime({
     duration:2000
     
   });
+  
+}
+
+function kat(){
+    anime({
+        targets: '#Katwilliams',
+        translateX: [200],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+      });
+      anime({
+        targets: '#DaveChappelle',
+        translateX: [-550],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+      });
+      anime({
+        targets: '#EddieMurphy',
+        translateX: [-900],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+      });
+}
+function richard(){
+    anime({
+        targets: '#RichardPryor',
+        translateX: [500],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+      });
+      anime({
+        targets: '#DaveChappelle',
+        translateX: [-550],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+      });
+      anime({
+        targets: '#EddieMurphy',
+        translateX: [-900],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+      });
+      anime({
+        targets: '#Katwilliams',
+        translateX: [-300],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+      });
+}
+
+function eddie(){
+    anime({
+        targets: '#EddieMurphy',
+        translateX: [-400],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+      });
+      anime({
+        targets: '#Katwilliams',
+        translateX: [-300],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+      });
+      anime({
+        targets: '#DaveChappelle',
+        translateX: [-550],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+      });
 }
