22f55205dc60971913023fec26192a7818068409
COR Coding <ls>
2024-03-27 14:14:42 -0400

    Created SearchBar that linked with secondApi (Completed)

--- EventSearch.js
+++ EventSearch.js
@@ -1,40 +1,14 @@
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-if (event.target == modal) {
-     modal.style.display = "none";
-   }
-}
 // submitInput is my button
 const submitInput = document.getElementById("button");
 const userInput = document.getElementById("SearchBar");
 const eventContainer = document.getElementById("EventList");
 
 const fetchData = () => {
-  const inputValue = userInput.value;
-  // url is dynamic for the user input
-  const url = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=comedy&apikey=pjOyZm2wY8wpDMijKrvkbYyzGxigNnZH&city=${inputValue}`;
+        const inputValue = userInput.value;
+        // url is dynamic for the user input
+        const url = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=comedy&apikey=pjOyZm2wY8wpDMijKrvkbYyzGxigNnZH&city=${inputValue}`;
 
-// I want an empty div, for my next input or else it'll display previous/current information
+        // I want an empty div, for my next input or else it'll display previous/current information
   eventContainer.innerHTML = "";
 
   fetch(url)
@@ -45,7 +19,6 @@ const fetchData = () => {
       if (data._embedded && data._embedded.events) {
         data._embedded.events.forEach((events) => {
           const eventItem = document.createElement("div");
-
           const titleElement = document.createElement("p");
           const urlElement = document.createElement("p");
 
@@ -53,9 +26,9 @@ const fetchData = () => {
             titleElement.innerText = events.name;
           
 
-           //titleElement.innerText = events.url;
+           //titleElement.innerText = events.url; - this would grab the url of the events
 
-           //titleElement.innerText = events.info;
+           //titleElement.innerText = events.info; - this would give details about the events
 
           // to add the empty container
           eventItem.appendChild(titleElement);
@@ -73,5 +46,4 @@ const fetchData = () => {
 // submitting the user input 
 submitInput.addEventListener("click", () => {
   fetchData();
-});   
-
+});
