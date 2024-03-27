d78c4f62d8e67d96a1a02ede4c59c1cbb64ea285
COR Coding <corcoding@DESKTOP-QMOVT9L.localdomain>
2024-03-27 09:04:56 -0400

    added popup for comedians

--- testP.js
+++ testP.js
@@ -1,35 +1,72 @@
+// Comedian functions
+
+//Dave Chappelle
 function dave(){
-anime({
-    targets: '#DaveChappelle',
-    translateX: [-100],
-    delay: 500,
-    direction: 'alternate',
-    loop: 1,
-    duration:2000
+    //Animation
+    anime({
+        targets: '#DaveChappelle',
+        translateX: [-100],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+    });
+    anime({
+        targets: '#Katwilliams',
+        translateX: [-300],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+    });
+    anime({
+        targets: '#EddieMurphy',
+        translateX: [-900],
+        delay: 500,
+        direction: 'alternate',
+        loop: 1,
+        duration:2000
+        
+    });
+    fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=5')
+
+    .then(response => response.json())
+    .then(user => { 
+        console.log(user);
+    for(let i = 0; i < 1; i++) {
+        console.log(user.jokes)
+
+    let setup = document.querySelector(".modal-body>p");
+    setup.innerHTML = user.jokes[0].setup
+    let punchline = document.querySelector(".modal-body>span")
+    punchline.innerHTML = user.jokes[0].delivery
     
-  });
-  anime({
-    targets: '#Katwilliams',
-    translateX: [-300],
-    delay: 500,
-    direction: 'alternate',
-    loop: 1,
-    duration:2000
-    
-  });
-  anime({
-    targets: '#EddieMurphy',
-    translateX: [-900],
-    delay: 500,
-    direction: 'alternate',
-    loop: 1,
-    duration:2000
-    
-  });
+    // Popup
+
+    // When the user clicks the button, open the modal 
+    setTimeout(function() {
+    modal.style.display = "block";
+    },2000);
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+
+}   
+}
+})
   
 }
-
+//Kat Williams
 function kat(){
+    //Animation
     anime({
         targets: '#Katwilliams',
         translateX: [200],
@@ -57,8 +94,42 @@ function kat(){
         duration:2000
         
       });
+      fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=5')
+
+      .then(response => response.json())
+      .then(user => { 
+          console.log(user);
+      for(let i = 0; i < 1; i++) {
+          console.log(user.jokes)
+
+      let setup = document.querySelector(".modal-body>p");
+      setup.innerHTML = user.jokes[0].setup
+      let punchline = document.querySelector(".modal-body>span")
+      punchline.innerHTML = user.jokes[0].delivery
+      
+      // Popup
+
+      // When the user clicks the button, open the modal 
+      setTimeout(function() {
+      modal.style.display = "block";
+      },2000);
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+      modal.style.display = "none";
+      }
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+
+}   
 }
+})
+}
+//Richard Pryor
 function richard(){
+    //Animation
     anime({
         targets: '#RichardPryor',
         translateX: [500],
@@ -95,9 +166,42 @@ function richard(){
         duration:2000
         
       });
-}
+      fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=5')
 
+      .then(response => response.json())
+      .then(user => { 
+          console.log(user);
+      for(let i = 0; i < 1; i++) {
+          console.log(user.jokes)
+
+      let setup = document.querySelector(".modal-body>p");
+      setup.innerHTML = user.jokes[0].setup
+      let punchline = document.querySelector(".modal-body>span")
+      punchline.innerHTML = user.jokes[0].delivery
+      
+      // Popup
+
+      // When the user clicks the button, open the modal 
+      setTimeout(function() {
+      modal.style.display = "block";
+      },2000);
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+      modal.style.display = "none";
+      }
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+
+}   
+}
+})
+}
+//Eddie Murphy
 function eddie(){
+ //Animation
     anime({
         targets: '#EddieMurphy',
         translateX: [-400],
@@ -125,30 +229,49 @@ function eddie(){
         duration:2000
         
       });
+      fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&amount=5')
+
+        .then(response => response.json())
+        .then(user => { 
+            console.log(user);
+        for(let i = 0; i < 1; i++) {
+            console.log(user.jokes)
+
+        let setup = document.querySelector(".modal-body>p");
+        setup.innerHTML = user.jokes[0].setup
+        let punchline = document.querySelector(".modal-body>span")
+        punchline.innerHTML = user.jokes[0].delivery
+        
+        // Popup
+
+        // When the user clicks the button, open the modal 
+        setTimeout(function() {
+        modal.style.display = "block";
+        },2000);
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+        modal.style.display = "none";
+        }
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+ 
+}   
 }
+})
+};
 
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("EddieMurphy");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-if (event.target == modal) {
-     modal.style.display = "none";
-   }
-}
+
