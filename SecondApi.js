1f82ea77bf6c096ee361e7b35a532b39b676af35
COR Coding <ls>
2024-03-28 11:11:26 -0400

    Removed extra files

--- SecondApi.js
+++
@@ -1,6 +0,0 @@
-// The second API was fetched using the ticketmaster deveploper tool
-//This is the link is a reference for the parameters:
-//https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/
-//Including the parameter for city will display a variety of options. So that has been updated and included below// 
-
-fetch('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=comedy&City=Atlanta&apikey=pjOyZm2wY8wpDMijKrvkbYyzGxigNnZH')
